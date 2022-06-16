436eb83dd43256892263150cf93c21bcd74718ce
fmontesa <fernandomontesa@yahoo.es>
2022-06-16 09:33:16 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,14 +30,7 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
-    },
-    {
-    resolve: `gatsby-source-contentful`,
-    options: {
-    spaceId: process.env.CONTENTFUL_SPACE_ID,
-    // Learn about environment variables: https://gatsby.dev/env-vars
-    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-    },    
+    }, 
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
